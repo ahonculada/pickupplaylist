3440e01595f7501263c2135b0f892124b154ca00
Mark Bruckert <markbruckert1@Gmail.com>
2021-10-16 22:07:37 -0400

    Adding Tags

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,11 +18,25 @@ export default function Home() {
             <div className="p-4">
               <h2 className="font-bold text-xl">Video Title</h2>
             </div>
-          </div>
-          <div className="bg-gray-100 rounded-xl">
-            <div className="w-full h-52 bg-gray-300 rounded-t-xl"></div>
-            <div className="p-4">
-              <h2 className="font-bold text-xl">Video Title</h2>
+            <div className="grid grid-cols-4 gap-4 ml-6 mr-6 mb-6">
+              <div className="bg-blue-200 border-blue-600 border-2 border-solid text-center text-blue-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-red-200 border-red-600 border-2 border-solid text-center text-red-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-green-200 border-green-600 border-2 border-solid text-center text-green-600 rounded-3xl font-bold">
+                Functions
+              </div>
+              <div className="bg-pink-200 border-pink-600 border-2 border-solid text-center text-pink-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-yellow-200 border-yellow-600 border-2 border-solid text-center text-yellow-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-indigo-200 border-indigo-600 border-2 border-solid text-center text-indigo-600 rounded-3xl font-bold">
+                Functions
+              </div>
             </div>
           </div>
           <div className="bg-gray-100 rounded-xl">
@@ -30,11 +44,25 @@ export default function Home() {
             <div className="p-4">
               <h2 className="font-bold text-xl">Video Title</h2>
             </div>
-          </div>
-          <div className="bg-gray-100 rounded-xl">
-            <div className="w-full h-52 bg-gray-300 rounded-t-xl"></div>
-            <div className="p-4">
-              <h2 className="font-bold text-xl">Video Title</h2>
+            <div className="grid grid-cols-4 gap-4 ml-6 mr-6 mb-6">
+              <div className="bg-blue-200 border-blue-600 border-2 border-solid text-center text-blue-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-red-200 border-red-600 border-2 border-solid text-center text-red-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-green-200 border-green-600 border-2 border-solid text-center text-green-600 rounded-3xl font-bold">
+                Functions
+              </div>
+              <div className="bg-pink-200 border-pink-600 border-2 border-solid text-center text-pink-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-yellow-200 border-yellow-600 border-2 border-solid text-center text-yellow-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-indigo-200 border-indigo-600 border-2 border-solid text-center text-indigo-600 rounded-3xl font-bold">
+                Functions
+              </div>
             </div>
           </div>
           <div className="bg-gray-100 rounded-xl">
@@ -42,11 +70,103 @@ export default function Home() {
             <div className="p-4">
               <h2 className="font-bold text-xl">Video Title</h2>
             </div>
+            <div className="grid grid-cols-4 gap-4 ml-6 mr-6 mb-6">
+              <div className="bg-blue-200 border-blue-600 border-2 border-solid text-center text-blue-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-red-200 border-red-600 border-2 border-solid text-center text-red-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-green-200 border-green-600 border-2 border-solid text-center text-green-600 rounded-3xl font-bold">
+                Functions
+              </div>
+              <div className="bg-pink-200 border-pink-600 border-2 border-solid text-center text-pink-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-yellow-200 border-yellow-600 border-2 border-solid text-center text-yellow-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-indigo-200 border-indigo-600 border-2 border-solid text-center text-indigo-600 rounded-3xl font-bold">
+                Functions
+              </div>
+            </div>
           </div>
           <div className="bg-gray-100 rounded-xl">
             <div className="w-full h-52 bg-gray-300 rounded-t-xl"></div>
             <div className="p-4">
               <h2 className="font-bold text-xl">Video Title</h2>
+            </div>
+            <div className="grid grid-cols-4 gap-4 ml-6 mr-6 mb-6">
+              <div className="bg-blue-200 border-blue-600 border-2 border-solid text-center text-blue-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-red-200 border-red-600 border-2 border-solid text-center text-red-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-green-200 border-green-600 border-2 border-solid text-center text-green-600 rounded-3xl font-bold">
+                Functions
+              </div>
+              <div className="bg-pink-200 border-pink-600 border-2 border-solid text-center text-pink-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-yellow-200 border-yellow-600 border-2 border-solid text-center text-yellow-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-indigo-200 border-indigo-600 border-2 border-solid text-center text-indigo-600 rounded-3xl font-bold">
+                Functions
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 rounded-xl">
+            <div className="w-full h-52 bg-gray-300 rounded-t-xl"></div>
+            <div className="p-4">
+              <h2 className="font-bold text-xl">Video Title</h2>
+            </div>
+            <div className="grid grid-cols-4 gap-4 ml-6 mr-6 mb-6">
+              <div className="bg-blue-200 border-blue-600 border-2 border-solid text-center text-blue-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-red-200 border-red-600 border-2 border-solid text-center text-red-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-green-200 border-green-600 border-2 border-solid text-center text-green-600 rounded-3xl font-bold">
+                Functions
+              </div>
+              <div className="bg-pink-200 border-pink-600 border-2 border-solid text-center text-pink-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-yellow-200 border-yellow-600 border-2 border-solid text-center text-yellow-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-indigo-200 border-indigo-600 border-2 border-solid text-center text-indigo-600 rounded-3xl font-bold">
+                Functions
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-100 rounded-xl">
+            <div className="w-full h-52 bg-gray-300 rounded-t-xl"></div>
+            <div className="p-4">
+              <h2 className="font-bold text-xl">Video Title</h2>
+            </div>
+            <div className="grid grid-cols-4 gap-4 ml-6 mr-6 mb-6">
+              <div className="bg-blue-200 border-blue-600 border-2 border-solid text-center text-blue-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-red-200 border-red-600 border-2 border-solid text-center text-red-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-green-200 border-green-600 border-2 border-solid text-center text-green-600 rounded-3xl font-bold">
+                Functions
+              </div>
+              <div className="bg-pink-200 border-pink-600 border-2 border-solid text-center text-pink-600 rounded-3xl font-bold">
+                Math
+              </div>
+              <div className="bg-yellow-200 border-yellow-600 border-2 border-solid text-center text-yellow-600 rounded-3xl font-bold">
+                Algebra
+              </div>
+              <div className="bg-indigo-200 border-indigo-600 border-2 border-solid text-center text-indigo-600 rounded-3xl font-bold">
+                Functions
+              </div>
             </div>
           </div>
         </div>
