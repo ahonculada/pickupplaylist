18f35ff2a789e80aa7e881acce7a30dad78adcce
aaron <aaron@pop-os.localdomain>
2021-10-24 23:05:46 -0400

    begin database

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from typing import List, Optional
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def read_root():
@@ -13,5 +24,17 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-URL = 'https://www.youtube.com/watch?v=NrO0CJCbYLA'
-vid_id = URL.split("=")[1]
+@app.post("/users/", response_model=shcemas.User)
+def create_user(user: schemas.UserCreate, db: Session= Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+#URL = 'https://www.youtube.com/watch?v=NrO0CJCbYLA'
+#vid_id = URL.split("=")[1]
