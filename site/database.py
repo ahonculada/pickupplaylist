0864b14ee90682a2981dccf9ead741fe605c2b05
aaron <aaron@pop-os.localdomain>
2021-10-29 14:54:40 -0400

    update structure for database, establish connection with localhost database, add list of requirements for backend

--- site/database.py
+++ site/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://yxfjvyvj:wfO1j2tq2X1vKWs497h1RJ8tHBYqmcyy@fanny.db.elephantsql.com/yxfjvyvj"
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./fastapi.db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
